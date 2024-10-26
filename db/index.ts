996dd7508028bd0afdfc0680363ad325219e3114
Andrew Kuczynski <kuczynskiandrew@gmail.com>
2024-10-26 15:05:58 -0400

    rename DB

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import * as SQLite from "expo-sqlite";
-const expo = SQLite.openDatabaseSync("database.db");
+const expo = SQLite.openDatabaseSync("mydb.sqlite");
 
 import * as schema from "./schema";
 export const db = drizzle(expo, { schema });
